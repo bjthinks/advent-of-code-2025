f73e38c8b7db51006ae6b2ff3cdab2f892a8e204
Brian Johnson <bjthinks@gmail.com>
2025-12-29 16:13:45 -0800

    Solved day 3 part 1

--- app/day03.hs
+++ app/day03.hs
@@ -1,4 +1,14 @@
 module Main where
 
+getJoltage :: String -> Int
+getJoltage str =
+  let digits = map (read . \x -> [x]) str :: [Int]
+      tensDigit = maximum $ init digits
+      onesDigit = maximum $ tail $ dropWhile (<tensDigit) digits
+  in tensDigit * 10 + onesDigit
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  raw <- readFile "day03-input.txt"
+  let joltages = map getJoltage $ lines raw
+  print $ sum joltages
