f411685d02feaec69911f44f1cc0c2226bb82ad0
Brian Johnson <bjthinks@gmail.com>
2025-12-31 12:54:15 -0800

    Solved day 3 part 2

--- app/day03.hs
+++ app/day03.hs
@@ -1,14 +1,22 @@
 module Main where
 
-getJoltage :: String -> Int
-getJoltage str =
-  let digits = map (read . \x -> [x]) str :: [Int]
-      tensDigit = maximum $ init digits
-      onesDigit = maximum $ tail $ dropWhile (<tensDigit) digits
-  in tensDigit * 10 + onesDigit
+getJoltage :: Int -> String -> Int
+getJoltage numDigits str = read $ concat $ map show $
+  getJoltage' numDigits $ map (read . \x -> [x]) str
+
+getJoltage' :: Int -> [Int] -> [Int]
+getJoltage' numDigits digits =
+  let len = length digits
+      nextDigit = maximum $ take (len - numDigits + 1) digits
+      remainingString = tail $ dropWhile (<nextDigit) digits
+  in case numDigits of
+    0 -> []
+    _ -> nextDigit : getJoltage' (numDigits-1) remainingString
 
 main :: IO ()
 main = do
   raw <- readFile "day03-input.txt"
-  let joltages = map getJoltage $ lines raw
-  print $ sum joltages
+  let joltages2 = map (getJoltage 2) (lines raw)
+  print $ sum joltages2
+  let joltages12 = map (getJoltage 12) (lines raw)
+  print $ sum joltages12
