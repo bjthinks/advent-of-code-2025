622e15277e00adbee8b61fd9a05b2269bf58d693
Brian Johnson <bjthinks@gmail.com>
2026-01-03 10:51:56 -0800

    Solved day 4 part 2

--- app/day04.hs
+++ app/day04.hs
@@ -9,14 +9,28 @@ numRollsNear arr (x,y) = sum $ do
   let c = arr ! (x0,y0)
   return $ if c == '@' then 1 else 0
 
-numAccessible :: Array (Int,Int) Char -> (Int,Int) -> Int
-numAccessible arr (width,height) = sum $ do
+accessibleRolls :: Array (Int,Int) Char -> (Int,Int) -> [(Int,Int)]
+accessibleRolls arr (width,height) = do
   x <- [1..width]
   y <- [1..height]
   let c = arr ! (x,y)
   case c of
-    '@' -> return $ if numRollsNear arr (x,y) <= 4 then 1 else 0
-    _ -> return 0
+    '@' -> if numRollsNear arr (x,y) <= 4 then [(x,y)] else []
+    _ -> []
+
+numAccessible :: Array (Int,Int) Char -> (Int,Int) -> Int
+numAccessible arr (width,height) = length $ accessibleRolls arr (width,height)
+
+removeRolls :: Array (Int,Int) Char -> [(Int,Int)] -> Array (Int,Int) Char
+removeRolls arr rolls =
+  let updates = map (\x -> (x,'.')) rolls
+  in arr // updates
+
+removeAllRolls :: Array (Int,Int) Char -> (Int,Int) -> Int
+removeAllRolls arr (width,height) =
+  case accessibleRolls arr (width,height) of
+    [] -> 0
+    rolls -> length rolls + removeAllRolls (removeRolls arr rolls) (width,height)
 
 main :: IO ()
 main = do
@@ -31,3 +45,4 @@ main = do
       paper' = concat $ map (\(y,xs) -> map (\(x,c) -> ((y,x),c)) xs) paper
       arr = listArray ((0,0),(width+1,height+1)) (repeat '.') // paper'
   print $ numAccessible arr (width,height)
+  print $ removeAllRolls arr (width,height)
