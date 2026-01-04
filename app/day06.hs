6f274cf1b4d5aada5a4ec86dd4cfca6678250a7f
Brian Johnson <bjthinks@gmail.com>
2026-01-04 07:17:01 -0800

    Solved day 6 part 1

--- app/day06.hs
+++ app/day06.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import Data.List
+
+calculate :: [String] -> Int
+calculate strings =
+  let numbers = map read $ init strings
+      operator = case last strings of
+        "+" -> (+)
+        "*" -> (*)
+        _ -> undefined
+  in foldr1 operator numbers
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  raw <- readFile "day06-input.txt"
+  let input = map words $ lines raw
+      transposedInput = transpose input
+      answers = map calculate transposedInput
+  print $ sum answers
