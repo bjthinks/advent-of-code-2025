d271f9d577c629ed6ac55f05ea7e3b097e3414a8
Brian Johnson <bjthinks@gmail.com>
2026-01-03 16:50:16 -0800

    Solved day 5 part 1

--- app/day05.hs
+++ app/day05.hs
@@ -1,4 +1,21 @@
 module Main where
 
+parseRange :: String -> (Int, Int)
+parseRange str = (read (takeWhile (/='-') str),
+                  read (tail (dropWhile (/='-') str)))
+
+withinARange :: [(Int, Int)] -> Int -> Bool
+withinARange [] _ = False
+withinARange ((lo,hi):rs) value
+  | value >= lo && value <= hi = True
+  | otherwise = withinARange rs value
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  raw <- readFile "day05-input.txt"
+  let rawLines = lines raw
+      rawRanges = takeWhile (/="") rawLines
+      rawIDs = tail $ dropWhile (/="") rawLines
+      ranges = map parseRange rawRanges
+      ids = map read rawIDs
+  print $ length $ filter (withinARange ranges) ids
