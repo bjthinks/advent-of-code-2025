7058c6b4d4f3d12ceff07fd648b18a8a905f9124
Brian Johnson <bjthinks@gmail.com>
2026-01-31 12:46:46 -0800

    Solved day 9 part 1

--- app/day09.hs
+++ app/day09.hs
@@ -1,4 +1,23 @@
 module Main where
 
+type Tile = (Int, Int)
+
+parseTile :: String -> Tile
+parseTile s =
+  let x = read $ takeWhile (/=',') s
+      y = read $ tail $ dropWhile (/=',') s
+  in (x, y)
+
+calculateAreaAllPairs :: [Tile] -> [Int]
+calculateAreaAllPairs tiles = do
+  x <- tiles
+  y <- tiles
+  return $ abs (fst x - fst y + 1) * abs (snd x - snd y + 1)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  raw <- readFile "day09-input.txt"
+  let tileStrs = lines raw
+      tiles = map parseTile tileStrs
+      areas = calculateAreaAllPairs tiles
+  print $ maximum areas
